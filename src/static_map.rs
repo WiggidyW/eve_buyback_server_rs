31c4a631ebaa1b32a62755a09dd1b4fdf4861211
Wiggidy <rlr0017@auburn.edu>
2023-06-22 19:42:43 -0500

    Update to "2023-06-22 19:42:42"

--- src/static_map.rs
+++ src/static_map.rs
@@ -1,3 +1,9 @@
-pub const PRICE_VERSION: &str = "2023-06-20";
+pub const PRICE_VERSION: &str = "2023-06-22 19:42:42";
 
-pub static REGION_MAP: phf::Map<&str, phf::Map<u32, (u8, u8, u8, u64)>> = phf::phf_map! {};
+pub static MARKET_MAP: phf::Map<u64, &str> = phf::phf_map! {
+    1030049082711: "1DQ1-A",
+    60003760: "Jita",
+};
+
+pub static REGION_MAP: phf::Map<&str, phf::Map<u32, (u8, u8, u8, u64)>> = phf::phf_map! {
+};
